1bef3d864acf9b0ce2822f1e8cb8bedf6825367a
derFrickler <johannes@hermen.net>
2020-02-11 06:58:35 +0100

    Update main.ino

--- src/main.ino
+++ src/main.ino
@@ -1,3 +1,23 @@
+/*****************************************************************************
+
+     Copyright (c) 2020 Johannes Hermen www.der-frickler.net
+
+     This library is free software; you can redistribute it and/or modify it
+     under the terms of the GNU Lesser General Public License as published
+     by the Free Software Foundation; either version 2 of the License, or
+     (at your option) any later version.
+
+     This software is distributed in the hope that it will be useful, but
+     WITHOUT ANY WARRANTY; without even the implied warranty of
+     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+     Lesser General Public License for more details.
+
+     You should have received a copy of the GNU Lesser General Public
+     License along with this library; if not, write to the Free Software
+     Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+
+  *****************************************************************************/
 #include <Adafruit_GFX.h>    // Core graphics library
 #include <Adafruit_ST7735.h> // Hardware-specific library
 #include <SPI.h>
